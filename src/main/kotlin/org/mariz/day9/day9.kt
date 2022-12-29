181c75fbdfdcdb88988b1399ee947de4c15c6b99
daniel <daniel.huebscher@mariz.ch>
2022-12-29 23:38:31 +0100

    day 9: exercise 1

--- src/main/kotlin/org/mariz/day9/day9.kt
+++ src/main/kotlin/org/mariz/day9/day9.kt
@@ -11,13 +11,13 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val visitedCoords = mutableSetOf<Coord>()
-        var currentCoords = PairCoord(Coord(0,0), Coord(0,0))
+        var currentCoords = PairCoord(Coord(0, 0), Coord(0, 0))
         input.forEach {
-            val instruction = parseInstruction(it)
-            for (i in 1..instruction.distance) {
-                currentCoords = currentCoords.moveHead(directionDelta(instruction.direction))
-                if (currentCoords.calcDistance().tailMustMove()) {
-                    currentCoords = currentCoords.moveTail(currentCoords.calcTailMove(currentCoords.calcDistance()))
+            for (i in it.parseInstruction().translateInstruction()) {
+                currentCoords = currentCoords.moveHead(i)
+                val distance = currentCoords.calcDistance()
+                if (distance.tailMustMove()) {
+                    currentCoords = currentCoords.moveTail(distance.calcTailMove())
                 }
                 visitedCoords.add(currentCoords.tail)
             }
@@ -25,7 +25,7 @@ fun main() {
         return visitedCoords.size
     }
 
-    fun part2(input: List<String>) = 0
+    // fun part2(input: List<String>) = 0
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(testFile)
@@ -43,15 +43,13 @@ data class Coord(val x: Int, val y: Int)
 data class PairCoord(val head: Coord, val tail: Coord)
 data class Instruction(val direction: String, val distance: Int)
 
-// typealias Instruction = Pair<String, Int> // first = direction, second = distance
-
 fun PairCoord.calcDistance() = Delta(this.head.x - this.tail.x, this.head.y - this.tail.y)
 
 fun Delta.tailMustMove() = this.x.absoluteValue > 1 || this.y.absoluteValue > 1
 
-fun PairCoord.calcTailMove(distance: Delta): Delta = when {
-    distance.x.absoluteValue == 2 -> Delta(distance.x / 2, distance.y)
-    distance.y.absoluteValue == 2 -> Delta(distance.x, distance.y / 2)
+fun Delta.calcTailMove(): Delta = when {
+    this.x.absoluteValue == 2 -> Delta(this.x / 2, this.y)
+    this.y.absoluteValue == 2 -> Delta(this.x, this.y / 2)
     else -> throw IllegalArgumentException()
 }
 
@@ -65,14 +63,14 @@ fun PairCoord.moveHead(delta: Delta): PairCoord {
     return PairCoord(targetHead, this.tail)
 }
 
-fun parseInstruction(instruction: String): Instruction {
+fun String.parseInstruction(): Instruction {
     return Instruction(
-        instruction.substringBefore(" "),
-        instruction.substringAfter(" ").toInt()
+        this.substringBefore(" "),
+        this.substringAfter(" ").toInt()
     )
 }
 
-fun directionDelta(direction: String) = when (direction) {
+fun String.directionDelta() = when (this) {
     "R" -> Delta(1, 0)
     "L" -> Delta(-1, 0)
     "U" -> Delta(0, 1)
@@ -81,4 +79,4 @@ fun directionDelta(direction: String) = when (direction) {
 }
 
 fun Instruction.translateInstruction(): List<Delta> =
-    List(this.distance) { directionDelta(this.direction) }
+    List(this.distance) { this.direction.directionDelta() }
